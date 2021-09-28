9315b3b259c723863d269b82702a4d192d31255c
lucca <67028657+luccagalhato@users.noreply.github.com>
2021-09-28 16:06:58 -0300

    update datatable sequency

--- main.go
+++ main.go
@@ -4,13 +4,15 @@ import (
 	"GoogleMAPS/api"
 	c "GoogleMAPS/config"
 	"GoogleMAPS/sql"
+	"embed"
 	"flag"
+	"io/fs"
 	"log"
 	"net/http"
-	"time"
-
-	"github.com/gorilla/mux"
 )
+
+//go:embed html
+var content embed.FS
 
 //const apiKey = "AIzaSyDybcJ7PHZPP2es7YN_hd0D5OWjIR3kue0"
 var createConfig bool
@@ -36,31 +38,30 @@ func main() {
 		return
 	}
 	sql.SetSQLConnLinx(connection)
+	htmlFS, err := fs.Sub(content, "html")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
 	log.Printf("starting server '%s' at port: %s", c.Config.API.Host, c.Config.API.Port)
 
-	r := mux.NewRouter()
-	r.PathPrefix("/html/").Handler(http.StripPrefix("/html/", http.FileServer(http.Dir("html"))))
-	r.Path("/request").Methods(http.MethodPost).HandlerFunc(api.ClientNew)
-
-	r.Path("/").Methods(http.MethodGet).HandlerFunc(redirect)
-	server := &http.Server{
-		Handler:      r,
-		Addr:         ":" + c.Config.API.Port,
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
-	log.Fatal(server.ListenAndServe())
+	http.HandleFunc("/request", api.ClientNew)
+	fs := http.FileServer(http.FS(htmlFS))
+	http.Handle("/html/", http.StripPrefix("/html/", fs))
+	log.Fatal(http.ListenAndServe(":8082", nil))
 	// connection.disconnect()ClientNew
 }
-func redirect(w http.ResponseWriter, req *http.Request) {
-	// remove/add not default ports from req.Host
-	target := "http://" + req.Host + "/html/register.html"
-	// log.Println(target)
-	if len(req.URL.RawQuery) > 0 {
-		target += "?" + req.URL.RawQuery
-	}
-	// log.Printf("redirect to: %s", target)
-	http.Redirect(w, req, target,
-		// see comments below and consider the codes 308, 302, or 301
-		http.StatusTemporaryRedirect)
-}
+
+// func redirect(w http.ResponseWriter, req *http.Request) {
+// 	// remove/add not default ports from req.Host
+// 	target := "http://" + req.Host + "/html/register.html"
+// 	// log.Println(target)
+// 	if len(req.URL.RawQuery) > 0 {
+// 		target += "?" + req.URL.RawQuery
+// 	}
+// 	// log.Printf("redirect to: %s", target)
+// 	http.Redirect(w, req, target,
+// 		// see comments below and consider the codes 308, 302, or 301
+// 		http.StatusTemporaryRedirect)
+// }
